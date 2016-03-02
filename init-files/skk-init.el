10848fdf7226be7f71e284cdee7e13451e4969a0
Daisuke Inoue <hibariyashiki@gmail.com>
2016-03-02 12:35:40 +0900

    change cursor color according to shinji or kyuji.

--- init-files/skk-init.el
+++ init-files/skk-init.el
@@ -19,7 +19,7 @@
   (save-buffer)
   (skk-kakutei))
 
-(setq skk-cursor-hiragana-color "yellow3")
+
 (setq skk-cursor-katakana-color "green3")
 (setq skk-cursor-latin-color "grey30")
 (setq skk-isearch-mode-enable nil)
@@ -42,7 +42,7 @@
     (setq skk-large-jisyo (nth 0 newlub))
     (setq skk-jisyo (nth 1 newlub))
     (setq skk-backup-jisyo (nth 2 newlub))
-
+    (setq skk-cursor-hiragana-color "orange4")
 ;;;    (setq skk-large-jisyo "~/ldic_kyuji.dic")
 ;;;    (setq skk-jisyo "~/udic_kyuji.dic")
 ;;;    (setq skk-backup-jisyo "~/udic_kyuji.dic.BAK")
@@ -59,7 +59,7 @@
     (setq skk-large-jisyo (nth 0 newlub))
     (setq skk-jisyo (nth 1 newlub))
     (setq skk-backup-jisyo (nth 2 newlub))
-
+    (setq skk-cursor-hiragana-color "yellow3")
 ;;;    (setq skk-large-jisyo "~/ldic_shinji.dic")
 ;;;    (setq skk-jisyo "~/udic_shinji.dic")
 ;;;    (setq skk-backup-jisyo "~/udic_shinji.dic.BAK")
