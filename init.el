3bb37fa94043a74ebceac62f7f1a1a3decae65c4
Daisuke Inoue <hibariyashiki@gmail.com>
2016-03-03 15:25:59 +0900

    amended indentation.

--- init.el
+++ init.el
@@ -8,19 +8,19 @@
 (require 'el-get-init)
 
 (setq files-to-require
-	  (list
-	   'keybind-init
-	   'misc-init
-	   'my-function-init
-	   'alias-init
-	   'appearance-init
-	   'face-init
-	   'whitespace-init
-		 'shell-mode-init
-		 'skk-init
-     'edit-text-object
-     'web-mode-init
-		 ))
+      (list
+       'keybind-init
+       'misc-init
+       'my-function-init
+       'alias-init
+       'appearance-init
+       'face-init
+       'whitespace-init
+       'shell-mode-init
+       'skk-init
+       'edit-text-object
+       'web-mode-init
+       ))
 
 (mapcar 'require files-to-require)
 
