2fe2dc04709b10641dce96ab94787b2fffadb644
Daisuke Inoue <hibariyashiki@gmail.com>
2016-06-03 23:24:45 +0900

    edit .php in web-mode.

--- init-files/web-mode-init.el
+++ init-files/web-mode-init.el
@@ -1,5 +1,6 @@
 (add-to-list 'auto-mode-alist '("\\.ctp\\'"   . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
 (add-hook 'web-mode-hook '(lambda ()
                             (setq web-mode-engines-alist
                               '(("php"    . "\\.ctp\\'"))
