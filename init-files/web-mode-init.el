2aaa7b3aafb0dad47c4bdcb14d55d6ea38d93f72
Daisuke Inoue <hibariyashiki@gmail.com>
2016-06-10 18:20:20 +0900

    css -> web-mode.

--- init-files/web-mode-init.el
+++ init-files/web-mode-init.el
@@ -1,10 +1,11 @@
 (add-to-list 'auto-mode-alist '("\\.ctp\\'"   . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
 (add-hook 'web-mode-hook '(lambda ()
-                            (setq web-mode-engines-alist
-                              '(("php"    . "\\.ctp\\'"))
-                              )
-                            ))
+                           (setq web-mode-engines-alist
+                            '(("php"    . "\\.ctp\\'"))
+                            )
+                           ))
 
 (provide 'web-mode-init)
