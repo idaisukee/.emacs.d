c84b2103e07049a937c28c144da55f9b2a4829ae
Daisuke Inoue <hibariyashiki@gmail.com>
2016-07-25 18:51:30 +0900

    web-mode indentation conf. ( does not work. )

--- init-files/web-mode-init.el
+++ init-files/web-mode-init.el
@@ -8,4 +8,21 @@
                             )
                            ))
 
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+ (setq web-mode-block-padding 0)
+ (setq web-mode-style-padding 0)
+ (setq web-mode-script-padding 0)
+
+ (setq web-mode-markup-indent-offset 0)
+ (setq web-mode-css-indent-offset 0)
+ (setq web-mode-code-indent-offset 0)
+
+ (add-to-list 'web-mode-indentation-params '("lineup-args" . nil))
+ (add-to-list 'web-mode-indentation-params '("lineup-calls" . nil))
+ (add-to-list 'web-mode-indentation-params '("lineup-concats" . nil))
+ (add-to-list 'web-mode-indentation-params '("lineup-ternary" . nil)))
+
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
 (provide 'web-mode-init)
