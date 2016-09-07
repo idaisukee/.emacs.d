8e262dbd7e090eac089320ebc07c36945ec3242c
Daisuke Inoue <hibariyashiki@gmail.com>
2016-09-07 17:53:40 +0900

    *.coffee -> coffee-mode.

--- init-files/misc-init.el
+++ init-files/misc-init.el
@@ -44,6 +44,7 @@
 (setq inhibit-splash-screen t)
 
 (add-to-list 'auto-mode-alist '("\\.less\\'" . css-mode))
+(add-to-list 'auto-mode-alist '("\\.coffee\\'" . coffee-mode))
 
 (if (string= emacs-version "24.5.1")
  (lambda ()
