4ff6567a3b8761d363e4cd62fece2be9b951e48c
Daisuke Inoue <hibariyashiki@gmail.com>
2017-06-14 20:05:42 +0900

    powerline face

--- init-files/powerline-init.el
+++ init-files/powerline-init.el
@@ -1,10 +1,45 @@
-(defface ieremii-face0
+(defface ieremii-aoi
  '((t
     (
-     :background "#ff00ff"
+     :background "#69d2e7"
      :inherit mode-line)))
   "ieremii face 0."
  :group 'powerline)
+
+(defface ieremii-clean-pondwater
+ '((t
+    (
+     :background "#a7dbd8"
+     :foreground "black"
+     :inherit mode-line)))
+  "ieremii face 0."
+ :group 'powerline)
+
+(defface ieremii-beach-storm
+ '((t
+    (
+     :background "#e0e4cc"
+     :inherit mode-line)))
+  "ieremii face 0."
+ :group 'powerline)
+
+
+(defface ieremii-giant-goldfish
+ '((t
+    (
+     :background "#f38630"
+     :inherit mode-line)))
+  "ieremii face 0."
+ :group 'powerline)
+
+(defface ieremii-unreal-food-pills
+ '((t
+    (
+     :background "#fa6900"
+     :inherit mode-line)))
+  "ieremii face 0."
+ :group 'powerline)
+
 
 (defface ieremii-face1
  '((t
@@ -33,9 +68,11 @@
        ((active (powerline-selected-window-active))
         (mode-line-buffer-id (if active 'mode-line-buffer-id 'mode-line-buffer-id-inactive))
         (mode-line (if active 'mode-line 'mode-line-inactive))
-        (face0 (if active 'ieremii-face0 'powerline-inactive1))
-        (face1 (if active 'ieremii-face1 'powerline-inactive2))
-        (face2 (if active 'ieremii-face2 'powerline-inactive2))
+        (face0 (if active 'ieremii-aoi 'powerline-inactive2))
+        (face1 (if active 'ieremii-clean-pondwater 'powerline-inactive2))
+        (face2 (if active 'ieremii-beach-storm 'powerline-inactive2))
+        (face3 (if active 'ieremii-giant-goldfish 'powerline-inactive2))
+        (face4 (if active 'ieremii-unreal-food-pills 'powerline-inactive1))
         (separator-left
          (intern
           (format "powerline-%s-%s"
@@ -50,39 +87,39 @@
          (list
           (powerline-raw "%*" face0 'l)
           (when powerline-display-buffer-size
-           (powerline-buffer-size face1 'l))
+           (powerline-buffer-size face0 'l))
           (when powerline-display-mule-info
-           (powerline-raw mode-line-mule-info face2 'l))
-          (powerline-raw "%f" face1)
+           (powerline-raw mode-line-mule-info face0 'l))
+          (powerline-raw "%f" face0)
           ;;          (powerline-buffer-id mode-line-buffer-id 'l)
           (when (and (boundp 'which-func-mode) which-func-mode)
            (powerline-raw which-func-format nil 'l))
-          (powerline-raw " " face0)
+          (powerline-raw " " face1)
           ;; (funcall separator-left mode-line face1)
           (when (and (boundp 'erc-track-minor-mode) erc-track-minor-mode)
            (powerline-raw erc-modified-channels-object face0 'l))
-          (powerline-major-mode face1 'l)
-          (powerline-process face2)
-          (powerline-narrow face1 'l)
-          (powerline-raw " " face0)
+          (powerline-major-mode face0 'l)
+          (powerline-process face0)
+          (powerline-narrow face0 'l)
+          (powerline-raw " " face1)
           ;;          (funcall separator-left face1 face2)
-          (powerline-vc face1 'r)
+          (powerline-vc face4 'r)
           (when (bound-and-true-p nyan-mode)
-           (powerline-raw (list (nyan-create)) face2 'l))))
+           (powerline-raw (list (nyan-create)) face0 'l))))
         (rhs
          (list
-          (powerline-raw global-mode-string face2 'r)
+          (powerline-raw global-mode-string face3 'r)
           ;;          (funcall separator-right face2 face1)
           (unless window-system
-           (powerline-raw (char-to-string #xe0a1) face1 'l))
-          (powerline-raw "%4l" face1 'l)
-          (powerline-raw ":" face1 'l)
-          (powerline-raw "%3c" face1 'r)
+           (powerline-raw (char-to-string #xe0a1) face4 'l))
+          (powerline-raw "%4l" face3 'l)
+          (powerline-raw ":" face3 'l)
+          (powerline-raw "%3c" face3 'r)
           ;;          (funcall separator-right face1 mode-line)
-          (powerline-raw " " face0)
-          (powerline-raw "%6p" face2 'r)
+          (powerline-raw " " face4)
+          (powerline-raw "%6p" face4 'r)
           (when powerline-display-hud
-           (powerline-hud face2 face1 2)))))
+           (powerline-hud face3 face2 2)))))
        (concat
         (powerline-render lhs)
         (powerline-fill face2 (powerline-width rhs))
