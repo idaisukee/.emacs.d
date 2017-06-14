cf4b091fe335ca2249c94a050a59815b67aee1c3
Daisuke Inoue <hibariyashiki@gmail.com>
2017-06-15 04:30:16 +0900

    del unnec lines.

--- init.el
+++ init.el
@@ -4,13 +4,13 @@
   (setq user-emacs-directory (file-name-directory load-file-name)))
 
 (add-to-list 'load-path "~/.emacs.d/")
-(add-to-list 'load-path "~/.emacs.d/site-lisp/")
 (add-to-list 'load-path "~/.emacs.d/init-files/")
 
 (require 'el-get-init)
 
 (setq files-to-require
  (list
+  'appearance-init
   'misc-init
   'my-function-init
   'alias-init
@@ -28,7 +28,5 @@
   'bind-key-init
   'yasnippet-init
   'powerline-init
-;;  'appearance-init
   ))
-(require 'appearance-init)
 (-map 'require files-to-require)
