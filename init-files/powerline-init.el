0ee6f95e56134d4e213daa8ccff3afa106f5084c
Daisuke Inoue <hibariyashiki@gmail.com>
2017-06-14 19:37:57 +0900

    powerline

--- init-files/powerline-init.el
+++ init-files/powerline-init.el
@@ -1,3 +1,28 @@
+(defface ieremii-face0
+ '((t
+    (
+     :background "#ff00ff"
+     :inherit mode-line)))
+  "ieremii face 0."
+ :group 'powerline)
+
+(defface ieremii-face1
+ '((t
+    (
+     :background "#bb00bb"
+     :inherit mode-line)))
+  "ieremii face 1."
+ :group 'powerline)
+
+(defface ieremii-face2
+ '((t
+    (
+     :background "#990099"
+     :inherit mode-line)))
+  "ieremii face 1."
+ :group 'powerline)
+
+
 (defun powerline-ieremii-theme ()
   "Setup the default mode-line."
   (interactive)
@@ -8,9 +33,9 @@
        ((active (powerline-selected-window-active))
         (mode-line-buffer-id (if active 'mode-line-buffer-id 'mode-line-buffer-id-inactive))
         (mode-line (if active 'mode-line 'mode-line-inactive))
-        (face1 (if active 'powerline-active1 'powerline-inactive1))
-        (face2 (if active 'powerline-active2 'powerline-inactive2))
-        (face3 (if active 'powerline-active2 'powerline-inactive2))
+        (face0 (if active 'ieremii-face0 'powerline-inactive1))
+        (face1 (if active 'ieremii-face1 'powerline-inactive2))
+        (face2 (if active 'ieremii-face2 'powerline-inactive2))
         (separator-left
          (intern
           (format "powerline-%s-%s"
@@ -23,26 +48,25 @@
            (cdr powerline-default-separator-dir))))
         (lhs
          (list
-          (powerline-raw "%*" mode-line 'l)
+          (powerline-raw "%*" face0 'l)
           (when powerline-display-buffer-size
-           (powerline-buffer-size mode-line 'l))
+           (powerline-buffer-size face1 'l))
           (when powerline-display-mule-info
-           (powerline-raw mode-line-mule-info mode-line 'l))
-          (powerline-raw "%f" face2)
+           (powerline-raw mode-line-mule-info face2 'l))
+          (powerline-raw "%f" face1)
           ;;          (powerline-buffer-id mode-line-buffer-id 'l)
           (when (and (boundp 'which-func-mode) which-func-mode)
            (powerline-raw which-func-format nil 'l))
-          (powerline-raw " ")
+          (powerline-raw " " face0)
           ;; (funcall separator-left mode-line face1)
           (when (and (boundp 'erc-track-minor-mode) erc-track-minor-mode)
-           (powerline-raw erc-modified-channels-object face1 'l))
+           (powerline-raw erc-modified-channels-object face0 'l))
           (powerline-major-mode face1 'l)
-          (powerline-process face1)
-          (powerline-minor-modes face1 'l)
+          (powerline-process face2)
           (powerline-narrow face1 'l)
-          (powerline-raw " " face1)
+          (powerline-raw " " face0)
           ;;          (funcall separator-left face1 face2)
-          (powerline-vc face2 'r)
+          (powerline-vc face1 'r)
           (when (bound-and-true-p nyan-mode)
            (powerline-raw (list (nyan-create)) face2 'l))))
         (rhs
@@ -55,8 +79,8 @@
           (powerline-raw ":" face1 'l)
           (powerline-raw "%3c" face1 'r)
           ;;          (funcall separator-right face1 mode-line)
-          (powerline-raw " ")
-          (powerline-raw "%6p" mode-line 'r)
+          (powerline-raw " " face0)
+          (powerline-raw "%6p" face2 'r)
           (when powerline-display-hud
            (powerline-hud face2 face1 2)))))
        (concat
