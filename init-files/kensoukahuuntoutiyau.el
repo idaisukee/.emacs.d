5c6ca26bf1b8b861dd987fc31fd71d5f55dad528
Daisuke Inoue <hibariyashiki@gmail.com>
2017-06-17 01:21:08 +0900

    ksu

--- init-files/kensoukahuuntoutiyau.el
+++ init-files/kensoukahuuntoutiyau.el
@@ -16,7 +16,17 @@
    (time (format-time-string "%s" (current-time)))
    (stdout (s-concat time ".stdout"))
    (stderr (s-concat time ".stderr")))
-  (shell-command (region-to-string (point-min) (point-max)) stdout stderr)))
+  (shell-command (region-to-string (point-min) (point-max)) stdout stderr)
+  (switch-to-buffer stdout)
+  (display-ansi-colors)
+  ))
+
+(require 'ansi-color)
+
+;; from https://stackoverflow.com/questions/23378271/how-do-i-display-ansi-color-codes-in-emacs-for-any-mode
+(defun display-ansi-colors ()
+  (interactive)
+  (ansi-color-apply-on-region (point-min) (point-max)))
 
 
 (provide 'kensoukahuuntoutiyau)
