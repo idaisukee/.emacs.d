047bd11054f0a2caef4ed335f0024c21c2bac2a9
Daisuke Inoue <hibariyashiki@gmail.com>
2017-08-03 17:18:21 +0900

    mode

--- init-files/auto-mode-init.el
+++ init-files/auto-mode-init.el
@@ -1,6 +1,6 @@
 (add-to-list 'auto-mode-alist '("\\.less\\'" . css-mode))
 (add-to-list 'auto-mode-alist '("\\.coffee\\'" . coffee-mode))
-(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
+
 (add-to-list 'auto-mode-alist '("\\.scratch\\'" . lisp-interaction-mode))
 (add-to-list 'auto-mode-alist '("\\.ejs\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.zsh\\'" . shell-script-mode))
@@ -8,5 +8,8 @@
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.shell\\'" . sh-mode))
-
+(if (string= (s-trim (shell-command-to-string "uname -n")) "ieremius-VirtualBox")
+    (add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
+    (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode)))
+    
 (provide 'auto-mode-init)
