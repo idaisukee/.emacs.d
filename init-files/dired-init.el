ddfe7137b27d7ad3efccf9aa86ea17be70e069f9
Daisuke Inoue <hibariyashiki@gmail.com>
2017-08-22 11:37:42 +0900

    dired switch

--- init-files/dired-init.el
+++ init-files/dired-init.el
@@ -5,4 +5,6 @@
 (add-hook 'dired-initial-position-hook 'dired-k)
 
 (add-hook 'dired-after-readin-hook #'dired-k-no-revert)
+
+(setq  dired-listing-switches "-al --time-style=long-iso")
 (provide 'dired-init)
